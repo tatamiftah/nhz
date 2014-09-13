75d1469628b6d9b62c5bf680929524cd3253d3da
tatamiftah <tata_nduts@yahoo.com>
2014-09-13 16:28:19 +0700

    Create hallmark.sh

---
+++ hallmark.sh
@@ -0,0 +1,9 @@
+yum install java-1.7.0-openjdk java-1.7.0-openjdk-devel -y
+sudo yum groupinstall Desktop -y
+sudo yum install tigervnc-server -y
+sudo yum install xorg-x11-fonts-Type1 -y
+sudo chkconfig vncserver on
+yum install nano -y
+wget https://github.com/NeXTHorizon/binaries/raw/master/nhz_v3.2.1.zip
+yum install unzip -y
+unzip nhz_v3.2.1
